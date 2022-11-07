5879f00d4126cf4967d5761b7723957049881c8e
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-11-06 23:13:25 -0300

    func-previous

--- script.js
+++ script.js
@@ -24,12 +24,33 @@ const next = (event) => {
   }
 }
 
+const previous = (event) => {
+  let secAtual = event.target.parentElement
+  secAtual.style.display = 'none'
+
+  for (let i=0; i<sessions.length; i+=1) {
+    if (sessions[i] === secAtual) {
+      sessions[i-1].style.display = 'flex'
+      break;
+    }
+  }
+}
+
 // Botões com nome 'próximo' são ligadas a func next
 
 for (let i=1; i<buttons.length; i+=1) {
   if (buttons[i].innerText.includes('Próximo')) {
     buttons[i].onclick = next;
-    console.log(buttons[i]);
+    // console.log(buttons[i]);
+  }
+}
+
+// Botões com nome 'anterior' são ligadas a func previous
+
+for (let i=1; i<buttons.length; i+=1) {
+  if (buttons[i].innerText.includes('Anterior')) {
+    buttons[i].onclick = previous;
+    // console.log(buttons[i]);
   }
 }
 
